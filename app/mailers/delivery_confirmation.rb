ec5dbbb093e32cfc36fa2f4b6a5ddf564be91913
Art Tucker <art.w.tucker@gmail.com>
2015-02-15 12:28:05 -0800

    Generate delivery confirmation mailer

---
+++ app/mailers/delivery_confirmation.rb
@@ -0,0 +1,2 @@
+class DeliveryConfirmation < ApplicationMailer
+end
---
+++ test/mailers/previews/delivery_confirmation_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/delivery_confirmation
+class DeliveryConfirmationPreview < ActionMailer::Preview
+
+end
